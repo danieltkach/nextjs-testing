049f36992979b2070b77cdc15257cef8966b9821
Daniel Tkach <daniel.tkach@dataart.com>
2023-05-06 21:34:26 -0300

    test refactor

--- base-concert-venue/__tests__/ui/band.test.tsx
+++ base-concert-venue/__tests__/ui/band.test.tsx
@@ -2,6 +2,10 @@ import { render, screen } from '@testing-library/react';
 import BandComponent from '@/pages/bands/[bandId]';
 import { readFakeData } from '@/__tests__/__mocks__/fakeData';
 
+function assert(element: HTMLElement) {
+  expect(element).toBeInTheDocument();
+}
+
 describe('band component displays correct band information', () => {
   beforeEach(async () => {
     const { fakeBands } = await readFakeData();
@@ -9,27 +13,20 @@ describe('band component displays correct band information', () => {
   });
 
   test('heading is correct', () => {
-    const heading = screen.getByRole('heading', {
-      name: /the wandering bunnies/i
-    });
-    expect(heading).toBeInTheDocument();
+    assert(screen.getByRole('heading', { name: /the wandering bunnies/i }));
   });
 
   test('band description is correct', () => {
-    const bandDescription = screen.getByText(/blistering world music, supported by a moody water glass orchestra/i);
-    expect(bandDescription).toBeInTheDocument();
+    assert(screen.getByText(/blistering world music, supported by a moody water glass orchestra/i));
   });
 
   test('band image comes up', () => {
-    const bandImage = screen.getByRole('img', {
-      name: /band photo/i
-    });
-    expect(bandImage).toBeInTheDocument();
+    assert(screen.getByRole('img', { name: /band photo/i }));
   });
 
   test('image footer has proper text and link', () => {
     const imageAuthor = screen.getByText(/Adina Voicu/i);
-    expect(imageAuthor).toBeInTheDocument();
+    assert(imageAuthor);
     expect(imageAuthor).toHaveAttribute('href', "https://pixabay.com/users/adinavoicu-485024/");
   });
 });
