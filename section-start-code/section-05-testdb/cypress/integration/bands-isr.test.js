c86ea9215329fb121f6bdfcdfc751b87a74fc697
Daniel Tkach <danieltkach@gmail.com>
2023-05-10 17:03:21 -0300

    Cypress ISR tests

---
+++ section-start-code/section-05-testdb/cypress/integration/bands-isr.test.js
@@ -0,0 +1,15 @@
+it('skips client-side bundle, confirming data from ISR cache', () => {
+  cy.task("db:reset");
+  
+  cy.request('/bands')
+    .its('body')
+    .then(html => {
+      const staticHtml = html.replace('/<script.*?>.*?<\/script>/gm', '');
+      cy.state('document').write(staticHtml);
+    });
+
+  cy.findAllByText(/avalanche of cheese/i).should("exist");
+  cy.findAllByText(/the joyous nun riot/i).should("exist");
+  cy.findAllByText(/shamrock pete/i).should("exist");
+  cy.findAllByText(/the wandering bunnies/i).should("exist");
+});
