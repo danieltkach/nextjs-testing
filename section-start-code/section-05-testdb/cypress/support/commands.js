ad05acd8aac6132dd1eec2972a16850e541e9aaf
Daniel Tkach <danieltkach@gmail.com>
2023-05-15 21:52:48 -0300

    already signed in test

--- section-start-code/section-05-testdb/cypress/support/commands.js
+++ section-start-code/section-05-testdb/cypress/support/commands.js
@@ -1,7 +1,22 @@
+/* eslint-disable cypress/unsafe-to-chain-command */
 import "@testing-library/cypress/add-commands";
 
 Cypress.Commands.add("resetDbAndIsrCache", () => {
   cy.task("db:reset");
   const secret = Cypress.env("REVALIDATION_SECRET");
   cy.request("GET", `/api/revalidate?secret=${secret}`);
+});
+
+Cypress.Commands.add("signIn", (email, password) => {
+  cy.visit("/auth/signin");
+  cy.findByLabelText(/email address/i)
+    .clear()
+    .type(email);
+  cy.findByLabelText(/password/i)
+    .clear()
+    .type(password);
+  cy.findByRole("main").within(() => {
+    cy.findByRole("button", { name: /sign in/i }).click();
+  });
+  cy.findByRole("heading", { name: /welcome/i }).should("exist");
 });
